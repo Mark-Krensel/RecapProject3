659e3dd1c87d40810a4902e2e3ed24dbca3ece53
Sukhpreet Singh <senti.kudi@yahoo.com>
2022-09-16 11:47:15 +0200

    add function createCharacterCard

--- components/card/card.js
+++ components/card/card.js
@@ -1,1 +1,28 @@
 export function createCharacterCard() {}
+
+function createCharacterCard(character) {
+  const card = document.createElement('li');
+
+  card.classList.add('card-container');
+
+  card.innerHTML = `<article class="card">
+<div class="card__image-container">
+    <img
+      src="${character.image}"
+      alt="FIND!"
+    />
+    <div class="class__image-gradient"></div>
+  </div>
+  <div class="card__content">
+<h2 class="card__title">${character.name}</h2>
+<dl class="card__info">
+<dt class="card__title">${character.title}</dt>
+<dd class="Card__info-title">${character.status}</dd>
+<dt class="card__info-title">${character.info}</dt>
+<dd class="card__description">${character.description}</dd>
+<dt class="card__info-title">${character.info}</dt>
+<dd class="card__info-description">${character.info}</dd>
+</dl>
+</div>
+</article>`;
+}
